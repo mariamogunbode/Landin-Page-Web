0169e5c963efff0b0d900ac4e17610960ef71aa6
Mariam <mariamogunbode@gmail.com>
2025-09-16 12:56:54 +0100

    removed HashRouter from App.jsx

--- buycar-project/src/App.jsx
+++ buycar-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router';
+import { Routes, Route } from 'react-router';
 import { EntireApp } from './assets/sections/EntireApp'
 import { NewestCar } from './assets/sections/NewestCar'
 import './App.css'
@@ -6,16 +6,11 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<EntireApp />} />
-          <Route path="cars" element={<NewestCar />} />
-        </Routes>
-      </HashRouter>
-
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<EntireApp />} />
+      <Route path="/cars" element={<NewestCar />} />
+    </Routes>
+  );
 }
 
 export default App
